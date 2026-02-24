5b3e0d9fe9163dcc53102da8bdcde7b0609a326b
RinEster <kliuevairina05@gmail.com>
2026-02-24 22:18:11 +0400

    feat(api): add delete category by id

--- yandex-project/src/api/categories.py
+++ yandex-project/src/api/categories.py
@@ -42,5 +42,18 @@ async def create_category(title: str, description : str, slug : str, is_publishe
 
     return new_category
 
-
+#удаление категории
+@categories_router.delete("/{category_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_category(category_id:int):
+    global categories
+    
+    for category in categories:
+        if category.id == category_id:
+            deleted = categories.remove(category)
+            return
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Категория не найдена"
+    )
 
