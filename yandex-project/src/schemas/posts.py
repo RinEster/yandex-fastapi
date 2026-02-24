b2fbd5730fdd1ada8c4a95f55a4fa877f4f884a9
RinEster <kliuevairina05@gmail.com>
2026-02-24 20:37:06 +0400

    feat: add comment model

--- yandex-project/src/schemas/posts.py
+++ yandex-project/src/schemas/posts.py
@@ -5,7 +5,7 @@ from schemas.categories import Category
 from schemas.locations import Location
 from datetime import datetime
 
-class PostSchema(BaseModel):
+class Post(BaseModel):
     title: str = Field(..., max_length=256, description='Заголовок')
     text: str = Field(..., description='Текст')
     pub_date: datetime = Field(..., description='Дата и время публикации')
@@ -14,4 +14,11 @@ class PostSchema(BaseModel):
     category: Category = Field(None,description='Категория')
     #image:
     is_published: bool = Field(True,description='Опубликовано')
+    created_at: datetime = Field(...,description='Добавлено')
+
+
+class Comment(BaseModel):
+    post: Post = Field(...,description='Публикация')
+    author: User = Field(...,description='Автор комментария')
+    text: str = Field(...,description='Текст комментария')
     created_at: datetime = Field(...,description='Добавлено')
