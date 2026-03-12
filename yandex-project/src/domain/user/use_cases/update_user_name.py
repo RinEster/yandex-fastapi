6f3fc002f067c2973fa363e128d205284309489b
RinEster <kliuevairina05@gmail.com>
2026-03-12 22:25:17 +0400

    fix(domain): fix update_user_name

--- yandex-project/src/domain/user/use_cases/update_user_name.py
+++ yandex-project/src/domain/user/use_cases/update_user_name.py
@@ -1,3 +1,4 @@
+from pydantic import SecretStr
 from infrastructure.sqlite.database import database
 from infrastructure.sqlite.repositories.users import UserRepository
 from schemas.users import User as UserSchema
@@ -7,12 +8,27 @@ class UpdateUserNameUseCase:
         self._database = database
         self._repo = UserRepository()
 
-    async def execute(self, user_id: int, new_first_name: str, new_second_name: str) -> UserSchema:
+    async def execute(
+        self,
+        user_id: int,
+        first_name: str | None = None,
+        second_name: str | None = None
+    ) -> UserSchema:
         with self._database.session() as session:
             user = self._repo.get_user_by_id(session, user_id)
             if not user:
                 raise ValueError("Пользователь не найден")
             
-            updated = self._repo.update_name(session, user_id, new_first_name, new_second_name)
+            updated = self._repo.update_name(session, user_id, first_name, second_name)
             
-            return UserSchema.model_validate(obj=updated)
+            user_dict = {
+                "id": updated.id,
+                "login": updated.login,
+                "email": updated.email,
+                "password": SecretStr(updated.password),
+                "first_name": updated.first_name,
+                "second_name": updated.second_name
+            }
+
+
+            return UserSchema.model_validate(obj=user_dict)
