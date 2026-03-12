e97a1e40eb2548ff02a3d0584cb0b47cded93201
RinEster <kliuevairina05@gmail.com>
2026-03-12 11:23:43 +0400

    feat(schemas): update posts

--- yandex-project/src/schemas/posts.py
+++ yandex-project/src/schemas/posts.py
@@ -4,15 +4,15 @@ from schemas.users import User
 from schemas.categories import Category
 from schemas.locations import Location
 from datetime import datetime
-
+#base 64 или папка с картинками
 class Post(BaseModel):
     title: str = Field(..., max_length=256, description='Заголовок')
     text: str = Field(..., description='Текст')
     pub_date: datetime = Field(..., description='Дата и время публикации')
     author: User = Field(...,description='Автор публикации')
-    location: Location = Field(None,description='Местоположение')
-    category: Category = Field(None,description='Категория')
-    #image:
+    location: Location | None = Field(None,description='Местоположение')
+    category: Category | None = Field(None,description='Категория')
+    image: str | None = Field(None, description="Путь к изображению")
     is_published: bool = Field(True,description='Опубликовано')
     created_at: datetime = Field(...,description='Добавлено')
 
