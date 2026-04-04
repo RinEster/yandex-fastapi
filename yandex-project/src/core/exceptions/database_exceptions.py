895614b084dc1e46c1b617f100b2843206fcfd25
RinEster <kliuevairina05@gmail.com>
2026-04-04 13:26:08 +0400

    feat(exceptions): add database exceptions

---
+++ yandex-project/src/core/exceptions/database_exceptions.py
@@ -0,0 +1,38 @@
+class BaseDatabaseException(Exception):
+    def __init__(self, detail: str | None = None) -> None:
+        self._detail = detail
+
+class UserNotFoundException(BaseDatabaseException):
+    pass
+
+
+class UserAlreadyExistsException(BaseDatabaseException):
+    pass
+
+
+class UserEmailAlreadyExistsException(BaseDatabaseException):
+    pass
+
+
+class PostNotFoundException(BaseDatabaseException):
+    pass
+
+
+class CategoryNotFoundException(BaseDatabaseException):
+    pass
+
+
+class CategorySlugAlreadyExistsException(BaseDatabaseException):
+    pass
+
+
+class LocationNotFoundException(BaseDatabaseException):
+    pass
+
+
+class LocationNameAlreadyExistsException(BaseDatabaseException):
+    pass
+
+
+class CommentNotFoundException(BaseDatabaseException):
+    pass
