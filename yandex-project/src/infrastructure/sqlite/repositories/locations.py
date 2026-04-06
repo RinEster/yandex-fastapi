bac0c34eee89e459777716c1fa924a156c1d8da1
RinEster <kliuevairina05@gmail.com>
2026-04-06 21:01:14 +0400

    feat(infrastructure): add database exceptions for locations

--- yandex-project/src/infrastructure/sqlite/repositories/locations.py
+++ yandex-project/src/infrastructure/sqlite/repositories/locations.py
@@ -5,12 +5,30 @@ from sqlalchemy.orm import Session
 
 from infrastructure.sqlite.models.locations import Location
 
-
+from core.exceptions.database_exceptions import(
+    LocationNameAlreadyExistsException,
+    LocationNotFoundException
+)
 class LocationRepository:
     def __init__(self):
         self._model: Type[Location] = Location
+    
+    def check_name_exists(
+        self,
+        session: Session,
+        name: str
+    )-> bool:
+        query = (
+            session.query(self._model)
+            .where(self._model.name == name)
+        )
+        location = query.scalar()
+        return location is not None
 
-    def get_all(self, session: Session) -> List[Location]:
+    def get_all(
+            self,
+            session: Session
+    ) -> List[Location]:
         query = session.query(self._model).all()
         return query
 
@@ -23,7 +41,10 @@ class LocationRepository:
             session.query(self._model)
             .where(self._model.id == location_id)
         )
-        return query.scalar()
+        location = query.scalar()
+        if not location:
+            raise LocationNotFoundException
+        return location
 
     def get_published(
         self,
@@ -41,6 +62,8 @@ class LocationRepository:
         name: str, 
         is_published: bool = True
     ) -> Location:
+        if self.check_name_exists(session, name):
+            raise LocationNameAlreadyExistsException
         location = self._model(
             name=name,
             is_published=is_published,
@@ -57,21 +80,20 @@ class LocationRepository:
         new_name: str
     ) -> Location:
         location = self.get_by_id(session,location_id)
-        if location:
-            location.name=new_name
-            session.flush()
+        
+        if location.name != new_name:
+            if self.check_name_exists(session, new_name):
+                raise LocationNameAlreadyExistsException
+        location.name=new_name
+        session.flush()
         return location
         
-    def delete_location(
+    def delete(
         self,
         session: Session,
         location_id: int
-    ) -> bool:
+    ) -> None:
         location = self.get_by_id(session, location_id)
-        if location:
-            session.delete(location)
-            session.flush()
-            return True
-        return False
-
+        session.delete(location)
+        session.flush()
 
