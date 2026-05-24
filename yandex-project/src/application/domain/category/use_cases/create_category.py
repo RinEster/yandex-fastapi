f986dc28be3243e32ba67c59bd2e02ebc24f0974
RinEster <kliuevairina05@gmail.com>
2026-05-24 14:00:39 +0400

    feat(domain): update create_category

--- yandex-project/src/application/domain/category/use_cases/create_category.py
+++ yandex-project/src/application/domain/category/use_cases/create_category.py
@@ -1,37 +1,46 @@
-from infrastructure.sqlite.database import database
-from infrastructure.sqlite.repositories.categories import CategoryRepository
-from schemas.categories import CategoryResponse, CategoryCreate, Category
-from fastapi import HTTPException, status
-from core.exceptions.database_exceptions import(
-    CategoryTitleAlreadyExistsException,
-    CategorySlugAlreadyExistsException
-)
-from core.exceptions.domain_exception import(
-    CategorySlugIsNotUniqueException,
-    CategoryTitleIsNotUniqueException
-)
 import logging
 
+from application.core.exceptions.database_exceptions import (
+    CategorySlugAlreadyExistsException,
+    CategoryTitleAlreadyExistsException,
+)
+from application.core.exceptions.domain_exception import (
+    CategorySlugIsNotUniqueException,
+    CategoryTitleIsNotUniqueException,
+)
+from application.infrastructure.postgres.database import database
+from application.infrastructure.postgres.repositories.categories import (
+    CategoryRepository,
+)
+from application.schemas.categories import (
+    CategoryCreate,
+    CategoryResponse,
+)
+
 logger = logging.getLogger(__name__)
+
 
 class CreateCategoryUseCase:
     def __init__(self):
         self._database = database
         self._repo = CategoryRepository()
 
-    async def execute(
-        self, 
-        data: CategoryCreate
-    ) -> CategoryResponse:
-        with self._database.session() as session:
+    async def execute(self, data: CategoryCreate) -> CategoryResponse:
+        async with self._database.session() as session:
             try:
-                category = self._repo.create(session=session,data=data)
+                category = await self._repo.create(
+                    session=session, data=data
+                )
             except CategoryTitleAlreadyExistsException:
-                error = CategoryTitleIsNotUniqueException(title=data.title)
+                error = CategoryTitleIsNotUniqueException(
+                    title=data.title
+                )
                 logger.error(error.get_detail())
                 raise error
             except CategorySlugAlreadyExistsException:
-                error = CategorySlugIsNotUniqueException(slug=data.slug)
+                error = CategorySlugIsNotUniqueException(
+                    slug=data.slug
+                )
                 logger.error(error.get_detail())
                 raise error
             return CategoryResponse.model_validate(obj=category)
