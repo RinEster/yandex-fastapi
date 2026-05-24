65eb14a4e03436b75b00c57216bb0902eaacaecd
RinEster <kliuevairina05@gmail.com>
2026-05-24 12:05:56 +0400

    feat(schemas): update categories

--- yandex-project/src/application/schemas/categories.py
+++ yandex-project/src/application/schemas/categories.py
@@ -1,23 +1,54 @@
-from pydantic import BaseModel, Field, ConfigDict
 from datetime import datetime
 
-class Category(BaseModel):
-    title:str = Field(...,max_length=256, description='Заголовок')
-    description: str = Field(..., description='Описание')
-    slug: str = Field(..., description='Идентификатор страницы для URL')
-    is_published: bool = Field(True, description='Опубликовано')
-    created_at: datetime | None = Field(None, description='Добавлено')
+from pydantic import BaseModel, ConfigDict, Field
 
-class CategoryResponse(Category):
-    id: int
-    model_config = ConfigDict(from_attributes=True)
 
-class CategoryCreate(Category):
+class CategoryBase(BaseModel):
+    title: str = Field(
+        ...,
+        max_length=256,
+        description="Заголовок",
+    )
+    description: str = Field(..., description="Описание")
+    slug: str = Field(
+        ...,
+        max_length=100,
+        pattern=r"^[a-z0-9_-]+$",
+        description="Идентификатор страницы для URL",
+    )
+    is_published: bool = Field(
+        True, description="Опубликовано"
+    )
+
+
+class CategoryCreate(CategoryBase):
     pass
 
-class CategoryUpdate(BaseModel):
-    title:str | None = Field(None, max_length=256, description='Заголовок')
-    description: str | None = Field(None, description='Описание')
-    slug: str | None = Field(None, description='Идентификатор страницы для URL')
-    is_published: bool | None = Field(None, description='Опубликовано')
 
+class CategoryUpdate(BaseModel):
+    title: str | None = Field(
+        None,
+        max_length=256,
+        description="Заголовок",
+    )
+    description: str | None = Field(
+        None, description="Описание"
+    )
+    slug: str | None = Field(
+        None,
+        max_length=100,
+        pattern=r"^[a-z0-9_-]+$",
+        description="Идентификатор страницы для URL",
+    )
+    is_published: bool | None = Field(
+        None, description="Опубликовано"
+    )
+
+
+class CategoryResponse(CategoryBase):
+    id: int
+    created_at: datetime = Field(
+        ..., description="Добавлено"
+    )
+
+    model_config = ConfigDict(from_attributes=True)
