64d281f91369f69b63a3fc436a6b774df43b5295
RinEster <kliuevairina05@gmail.com>
2026-05-24 12:02:50 +0400

    feat(schemas): update users, add userUpdate

--- yandex-project/src/application/schemas/users.py
+++ yandex-project/src/application/schemas/users.py
@@ -31,17 +31,48 @@ class UserCreate(UserBase):
     ) -> SecretStr:
         raw_password = value.get_secret_value()
 
-        if not any(
-            char.isdigit()
-            for char in raw_password
-        ):
+        if not any(char.isdigit() for char in raw_password):
             raise ValueError(
                 "Пароль должен содержать хотя бы одну цифру"
             )
-        if not any(
-            char.isupper()
-            for char in raw_password
-        ):
+        if not any(char.isupper() for char in raw_password):
+            raise ValueError(
+                "Пароль должен содержать хотя бы одну заглавную букву"
+            )
+
+        return value
+
+
+class UserUpdate(BaseModel):
+    login: str | None = Field(None, max_length=50)
+    email: EmailStr | None = None
+    first_name: str | None = Field(
+        None, max_length=50, description="Имя"
+    )
+    second_name: str | None = Field(
+        None, max_length=50, description="Фамилия"
+    )
+
+    password: SecretStr | None = Field(
+        None, min_length=8, max_length=128
+    )
+
+    @field_validator("password")
+    @classmethod
+    def validate_password_complexity(
+        cls, value: SecretStr | None
+    ) -> SecretStr | None:
+        if value is None:
+            return value
+
+        raw_password = value.get_secret_value()
+
+        if not any(char.isdigit() for char in raw_password):
+            raise ValueError(
+                "Пароль должен содержать хотя бы одну цифру"
+            )
+
+        if not any(char.isupper() for char in raw_password):
             raise ValueError(
                 "Пароль должен содержать хотя бы одну заглавную букву"
             )
@@ -52,6 +83,4 @@ class UserCreate(UserBase):
 class UserResponse(UserBase):
     id: int
 
-    model_config = ConfigDict(
-        from_attributes=True
-    )
+    model_config = ConfigDict(from_attributes=True)
