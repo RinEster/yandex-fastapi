4b4da67fa12b473e4bc851567ce8d691e13a48da
RinEster <kliuevairina05@gmail.com>
2026-05-24 14:11:38 +0400

    feat(domain): update get_by_slug

--- yandex-project/src/application/domain/category/use_cases/get_by_slug.py
+++ yandex-project/src/application/domain/category/use_cases/get_by_slug.py
@@ -1,11 +1,19 @@
-from infrastructure.sqlite.database import database
-from infrastructure.sqlite.repositories.categories import CategoryRepository
-from schemas.categories import CategoryResponse
-from core.exceptions.database_exceptions import CategoryNotFoundException
-from core.exceptions.domain_exception import CategoryNotFoundBySlugException
 import logging
 
+from application.core.exceptions.database_exceptions import (
+    CategoryNotFoundException,
+)
+from application.core.exceptions.domain_exception import (
+    CategoryNotFoundBySlugException,
+)
+from application.infrastructure.postgres.database import database
+from application.infrastructure.postgres.repositories.categories import (
+    CategoryRepository,
+)
+from application.schemas.categories import CategoryResponse
+
 logger = logging.getLogger(__name__)
+
 
 class GetCategoryBySlugUseCase:
     def __init__(self):
@@ -13,12 +21,12 @@ class GetCategoryBySlugUseCase:
         self._repo = CategoryRepository()
 
     async def execute(self, slug: str) -> CategoryResponse:
-        with self._database.session() as session:
+        async with self._database.session() as session:
             try:
-                category = self._repo.get_by_slug(session, slug)
+                category = await self._repo.get_by_slug(session, slug)
             except CategoryNotFoundException:
                 error = CategoryNotFoundBySlugException(slug=slug)
                 logger.error(error.get_detail())
                 raise error
-            
+
             return CategoryResponse.model_validate(obj=category)
