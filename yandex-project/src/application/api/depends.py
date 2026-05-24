dbbb52b8e2c9b71640a173e848f5947be1551225
RinEster <kliuevairina05@gmail.com>
2026-05-24 15:41:13 +0400

    feat(api): update depends

--- yandex-project/src/application/api/depends.py
+++ yandex-project/src/application/api/depends.py
@@ -1,38 +1,42 @@
-from domain.user.use_cases.create_user import CreateUserUseCase
-from domain.user.use_cases.get_user_by_id import GetUserByIdUseCase
-from domain.user.use_cases.get_user_by_login import GetUserByLoginUseCase
-from domain.user.use_cases.get_user_by_email import GetUserByEmailUseCase
-from domain.user.use_cases.get_all_users import GetAllUsersUseCase
-from domain.user.use_cases.update_user_login import UpdateUserLoginUseCase
-from domain.user.use_cases.update_user_email import UpdateUserEmailUseCase
-from domain.user.use_cases.update_user_password import UpdateUserPasswordUseCase
-from domain.user.use_cases.update_user_name import UpdateUserNameUseCase
-from domain.user.use_cases.delete_user import DeleteUserUseCase
+from application.domain.user.use_cases.create_user import CreateUserUseCase
+from application.domain.user.use_cases.get_user_by_id import GetUserByIdUseCase
+from application.domain.user.use_cases.get_user_by_login import GetUserByLoginUseCase
+from application.domain.user.use_cases.get_user_by_email import GetUserByEmailUseCase
+from application.domain.user.use_cases.get_all_users import GetAllUsersUseCase
+from application.domain.user.use_cases.update_user import UpdateUserUseCase
+from application.domain.user.use_cases.delete_user import DeleteUserUseCase
 
-from domain.location.use_cases.create_location import CreateLocationUseCase
-from domain.location.use_cases.get_all_locations import GetAllLocationsUseCase
-from domain.location.use_cases.get_location_by_id import GetLocationByIdUseCase
-from domain.location.use_cases.get_published import GetPublishedLocationsUseCase
-from domain.location.use_cases.update_location import UpdateLocationUseCase
-from domain.location.use_cases.delete_location import DeleteLocationUseCase
+from application.domain.location.use_cases.create_location import CreateLocationUseCase
+from application.domain.location.use_cases.get_all_locations import GetAllLocationsUseCase
+from application.domain.location.use_cases.get_location_by_id import GetLocationByIdUseCase
+from application.domain.location.use_cases.get_published import GetPublishedLocationsUseCase
+from application.domain.location.use_cases.update_location import UpdateLocationUseCase
+from application.domain.location.use_cases.delete_location import DeleteLocationUseCase
+
+from application.domain.category.use_cases.create_category import CreateCategoryUseCase
+from application.domain.category.use_cases.get_all_categories import GetAllCategoriesUseCase
+from application.domain.category.use_cases.get_category_by_id import GetCategoryByIdUseCase
+from application.domain.category.use_cases.get_published import GetPublishedCategoriesUseCase
+from application.domain.category.use_cases.update_category import UpdateCategoryUseCase
+from application.domain.category.use_cases.delete_category import DeleteCategoryUseCase
+from application.domain.category.use_cases.get_by_slug import GetCategoryBySlugUseCase
+
+from application.domain.post.use_cases.get_all_posts import GetAllPostsUseCase
+from application.domain.post.use_cases.get_post_by_id import GetPostByIdUseCase
+from application.domain.post.use_cases.create_post import CreatePostUseCase
+from application.domain.post.use_cases.update_post import UpdatePostUseCase
+from application.domain.post.use_cases.delete_post import DeletePostUseCase
+from application.domain.post.use_cases.get_published import GetPublishedPostsUseCase
 
 
-from domain.category.use_cases.create_category import CreateCategoryUseCase
-from domain.category.use_cases.get_all_categories import GetAllCategoriesUseCase
-from domain.category.use_cases.get_category_by_id import GetCategoryByIdUseCase
-from domain.category.use_cases.get_published import GetPublishedCategoriesUseCase
-from domain.category.use_cases.update_category import UpdateCategoryUseCase
-from domain.category.use_cases.delete_category import DeleteCategoryUseCase
-from domain.category.use_cases.get_by_slug import GetCategoryBySlugUseCase
+from application.domain.comment.use_cases.create_comment import CreateCommentUseCase
+from application.domain.comment.use_cases.get_comment_by_id import GetCommentByIdUseCase
+from application.domain.comment.use_cases.get_comment_by_post import GetCommentsByPostUseCase
+from application.domain.comment.use_cases.update_comment import UpdateCommentUseCase
+from application.domain.comment.use_cases.delete_comment import DeleteCommentUseCase
 
-from domain.posts.use_cases.get_all_posts import GetAllPostsUseCase
-from domain.posts.use_cases.get_post_by_id import GetPostByIdUseCase
-from domain.posts.use_cases.create_post import CreatePostUseCase
-from domain.posts.use_cases.update_post import UpdatePostUseCase
-from domain.posts.use_cases.delete_post import DeletePostUseCase
-
-from domain.auth.use_cases.authenticate_user import AuthenticateUserUseCase
-from domain.auth.use_cases.create_access_token import CreateAccessTokenUseCase
+from application.domain.auth.use_cases.authenticate_user import AuthenticateUserUseCase
+from application.domain.auth.use_cases.create_access_token import CreateAccessTokenUseCase
 
 
 def authenticate_user_use_case() -> AuthenticateUserUseCase:
@@ -41,25 +45,26 @@ def authenticate_user_use_case() -> AuthenticateUserUseCase:
 def create_access_token_use_case() -> CreateAccessTokenUseCase:
     return CreateAccessTokenUseCase()
 
-def get_get_all_posts_use_case() -> GetAllPostsUseCase:
+
+def get_all_posts_use_case() -> GetAllPostsUseCase:
     return GetAllPostsUseCase()
 
-
-def get_get_post_by_id_use_case() -> GetPostByIdUseCase:
+def post_by_id_use_case() -> GetPostByIdUseCase:
     return GetPostByIdUseCase()
 
-
-def get_create_post_use_case() -> CreatePostUseCase:
+def create_post_use_case() -> CreatePostUseCase:
     return CreatePostUseCase()
 
-
-def get_update_post_use_case() -> UpdatePostUseCase:
+def update_post_use_case() -> UpdatePostUseCase:
     return UpdatePostUseCase()
 
-
-def get_delete_post_use_case() -> DeletePostUseCase:
+def delete_post_use_case() -> DeletePostUseCase:
     return DeletePostUseCase()
 
+
+
+def get_published_post_use_case() -> GetPublishedPostsUseCase:
+    return GetPublishedPostsUseCase()
 
 def create_category_use_case() -> CreateCategoryUseCase:
     return CreateCategoryUseCase()
@@ -70,42 +75,51 @@ def get_category_by_slug_use_case() -> GetCategoryBySlugUseCase:
 def get_all_categories_use_case() -> GetAllCategoriesUseCase:
     return GetAllCategoriesUseCase()
 
-
 def get_category_by_id_use_case() -> GetCategoryByIdUseCase:
     return GetCategoryByIdUseCase()
-
 
 def get_published_categories_use_case() -> GetPublishedCategoriesUseCase:
     return GetPublishedCategoriesUseCase()
 
-
 def update_category_use_case() -> UpdateCategoryUseCase:
     return UpdateCategoryUseCase()
-
 
 def delete_category_use_case() -> DeleteCategoryUseCase:
     return DeleteCategoryUseCase()
 
 
+
+def create_comment_use_case() -> CreateCommentUseCase:
+    return CreateCommentUseCase()
+
+def get_comment_by_id_use_case() -> GetCommentByIdUseCase:
+    return GetCommentByIdUseCase()
+
+def get_comments_by_post_use_case() -> GetCommentsByPostUseCase:
+    return GetCommentsByPostUseCase()
+
+def update_comment_use_case() -> UpdateCommentUseCase:
+    return UpdateCommentUseCase()
+
+def delete_comment_use_case() -> DeleteCommentUseCase:
+    return DeleteCommentUseCase()
+
+
+
 def create_location_use_case() -> CreateLocationUseCase:
     return CreateLocationUseCase()
-
 
 def get_all_locations_use_case() -> GetAllLocationsUseCase:
     return GetAllLocationsUseCase()
 
-
 def get_location_by_id_use_case() -> GetLocationByIdUseCase:
     return GetLocationByIdUseCase()
-
 
 def get_published_locations_use_case() -> GetPublishedLocationsUseCase:
     return GetPublishedLocationsUseCase()
 
-
 def update_location_use_case() -> UpdateLocationUseCase:
     return UpdateLocationUseCase()
-
 
 def delete_location_use_case() -> DeleteLocationUseCase:
     return DeleteLocationUseCase()
@@ -126,17 +140,8 @@ def get_user_by_email_use_case() -> GetUserByEmailUseCase:
 def get_all_users_use_case() -> GetAllUsersUseCase:
     return GetAllUsersUseCase()
 
-def update_user_login_use_case() -> UpdateUserLoginUseCase:
-    return UpdateUserLoginUseCase()
-
-def update_user_email_use_case() -> UpdateUserEmailUseCase:
-    return UpdateUserEmailUseCase()
-
-def update_user_password_use_case() -> UpdateUserPasswordUseCase:
-    return UpdateUserPasswordUseCase()
-
-def update_user_name_use_case() -> UpdateUserNameUseCase:
-    return UpdateUserNameUseCase()
+def update_user_use_case() -> UpdateUserUseCase:
+    return UpdateUserUseCase()
 
 def delete_user_use_case() -> DeleteUserUseCase:
     return DeleteUserUseCase()
